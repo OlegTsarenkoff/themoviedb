ea1ad040af98ddc74aa7264218993f6b1fffff8a
Oleg Tsarenkoff <tsarenkow@gmail.com>
2021-12-13 12:08:19 +0300

    Monday commit, add validater from TF

--- lib/widgets/auth/auth_widget.dart
+++ lib/widgets/auth/auth_widget.dart
@@ -2,6 +2,9 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:themoviedb/Theme/app_button_style.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'info_block_wrapper.dart';
+
+enum LinkTabs { theBasics, getInvolved, community, legal }
 
 class AuthWidget extends StatefulWidget {
   const AuthWidget({Key? key}) : super(key: key);
@@ -16,7 +19,9 @@ class _AuthWidgetState extends State<AuthWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
           leading: _buildMenuActionButton(_onTapMenu, Icons.menu),
           centerTitle: true,
           title: Padding(
@@ -29,43 +34,59 @@ class _AuthWidgetState extends State<AuthWidget> {
           ),
           actions: [
             _buildMenuActionButton(_onTapProfile, Icons.person),
-            _buildMenuActionButton(_onTapSearch, Icons.search),
+            _buildMenuActionButton(_onTapSearch, Icons.search,
+                color: Colors.blue),
             const SizedBox(
               width: 5,
             )
           ],
         ),
-        body: ListView(
-          children: const [_HeaderWidget()],
-        ));
+        SliverToBoxAdapter(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                _HeaderWidget(),
+                Container(
+                  height: 750,
+                  width: double.infinity,
+                  child: _buildInfoSection(),
+                ),
+                const Center(
+                  child: Text('build: a0.1.10.12.21'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
 
-_buildMenuActionButton(
-  Function() onPressed,
-  IconData icon,
-) =>
-    IconButton(
-      onPressed: onPressed,
-      icon: Icon(icon),
-      splashRadius: 20,
+_buildInfoSection() => Padding(
+      padding: const EdgeInsets.only(left: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 40,
+          ),
+          _buildJoinButton(),
+          const SizedBox(
+            height: 40,
+          ),
+          InfoBlockWrapper(LinkTabs.values),
+        ],
+      ),
     );
 
-////////////////////////////////////////////////////////////////////////////////
-_onTapProfile() {
-  print('profile');
-}
+_buildJoinButton() => ElevatedButton(
+      child: Text('Joun the community'.toUpperCase()),
+      style: AppButtonStyle.communityButton,
+      onPressed: () {},
+    );
 
-_onTapSearch() {
-  print('search');
-}
-
-_onTapMenu() {
-  print('menu');
-}
-////////////////////////////////////////////////////////////////////////////////
-
-//Header (text, forms/buttons, )
+// Header (text, forms/buttons, )
 class _HeaderWidget extends StatelessWidget {
   const _HeaderWidget({Key? key}) : super(key: key);
 
@@ -73,88 +94,88 @@ class _HeaderWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     const textStyle = TextStyle(color: Colors.black, fontSize: 16);
     const accentColor = Color(0xFF01B4E4);
-    var l = [
-      1,
-      ...[2, 32]
-    ];
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Login to your account',
-            style: TextStyle(
-              fontSize: 23,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
+
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Login to your account',
+              style: TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          RichText(
-            text: TextSpan(children: [
-              const TextSpan(
-                text:
-                    "In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple. ",
-                style: textStyle,
-              ),
-              TextSpan(
-                text: "Click here",
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: accentColor,
-                  decoration: TextDecoration.underline,
+            const SizedBox(
+              height: 10,
+            ),
+            RichText(
+              text: TextSpan(children: [
+                const TextSpan(
+                  text:
+                      "In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple. ",
+                  style: textStyle,
                 ),
-                recognizer: TapGestureRecognizer()
-                  ..onTap = () {
-                    print('hikhk');
-                  }, // need link to registred
-              ),
-              const TextSpan(
-                text: " to get started.",
-                style: textStyle,
-              ),
-            ]),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          RichText(
-            text: TextSpan(children: [
-              const TextSpan(
-                text:
-                    "If you signed up but didn`t get your verification email, ",
-                style: textStyle,
-              ),
-              TextSpan(
-                text: "click here",
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: accentColor,
-                  decoration: TextDecoration.underline,
+                TextSpan(
+                  text: "Click here",
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: accentColor,
+                    decoration: TextDecoration.underline,
+                  ),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      print('hikhk');
+                    }, // need link to registred
                 ),
-                recognizer: TapGestureRecognizer()
-                  ..onTap = null, // need link to reset password
-              ),
-              const TextSpan(
-                text: " to have it resent.",
-                style: textStyle,
-              ),
-            ]),
-          ),
-          const SizedBox(
-            height: 35,
-          ),
-          const _FormWidget(),
-        ],
+                const TextSpan(
+                  text: " to get started.",
+                  style: textStyle,
+                ),
+              ]),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            RichText(
+              text: TextSpan(children: [
+                const TextSpan(
+                  text:
+                      "If you signed up but didn`t get your verification email, ",
+                  style: textStyle,
+                ),
+                TextSpan(
+                  text: "click here",
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: accentColor,
+                    decoration: TextDecoration.underline,
+                  ),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = null, // need link to reset password
+                ),
+                const TextSpan(
+                  text: " to have it resent.",
+                  style: textStyle,
+                ),
+              ]),
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            const _FormWidget(),
+          ],
+        ),
       ),
     );
   }
 }
 
-//Form (login/pass, buttons)
+// Form (login/pass, buttons)
 class _FormWidget extends StatefulWidget {
   const _FormWidget({Key? key}) : super(key: key);
 
@@ -163,6 +184,26 @@ class _FormWidget extends StatefulWidget {
 }
 
 class __FormWidgetState extends State<_FormWidget> {
+  final _loginTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+  String? errorText = null;
+
+  void _auth() {
+    final login = _loginTextController.text;
+    final password = _passwordTextController.text;
+
+    if (login == 'admin' && password == 'admin') {
+      errorText = null;
+    } else {
+      errorText = 'Incorrect login or password!';
+    }
+    setState(() {});
+  }
+
+  void _resetPassword() {
+    print("reset password");
+  }
+
   @override
   Widget build(BuildContext context) {
     const colorButton = Color(0xFF01B4E4);
@@ -175,7 +216,7 @@ class __FormWidgetState extends State<_FormWidget> {
       isCollapsed: true,
       contentPadding: EdgeInsets.symmetric(vertical: 10),
     );
-
+    final errorText = this.errorText;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -186,7 +227,10 @@ class __FormWidgetState extends State<_FormWidget> {
         const SizedBox(
           height: 5,
         ),
-        const TextField(decoration: textFieldDecorator),
+        TextField(
+          controller: _loginTextController,
+          decoration: textFieldDecorator,
+        ),
         const SizedBox(
           height: 25,
         ),
@@ -197,7 +241,8 @@ class __FormWidgetState extends State<_FormWidget> {
         const SizedBox(
           height: 5,
         ),
-        const TextField(
+        TextField(
+          controller: _passwordTextController,
           decoration: textFieldDecorator,
           obscureText: true,
         ),
@@ -206,8 +251,8 @@ class __FormWidgetState extends State<_FormWidget> {
         ),
         Row(
           children: [
-            TextButton(
-              onPressed: () => {}, // need link to HomePage
+            ElevatedButton(
+              onPressed: _auth, // need link to HomePage
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(colorButton),
                 foregroundColor: MaterialStateProperty.all(Colors.white),
@@ -224,13 +269,49 @@ class __FormWidgetState extends State<_FormWidget> {
               width: 30,
             ),
             TextButton(
-              onPressed: () => {}, // need link to RegistredPage
+              onPressed: _resetPassword, // need link to RegistredPage
               style: AppButtonStyle.linkButton,
               child: const Text('Reset password'),
             )
           ],
         ),
+        const SizedBox(
+          height: 5,
+        ),
+        if (errorText != null) ...[
+          Text(
+            errorText,
+            style: const TextStyle(
+              color: Colors.red,
+              fontSize: 17,
+            ),
+          ),
+        ],
       ],
     );
   }
 }
+
+// Functions
+_buildMenuActionButton(Function() onPressed, IconData icon, {Color? color}) =>
+    IconButton(
+      onPressed: onPressed,
+      icon: Icon(icon),
+      color: color ?? Colors.white,
+      splashRadius: 20,
+    );
+
+////////////////////////////////////////////////////////////////////////////////
+_onTapProfile() {
+  print('profile');
+}
+
+_onTapSearch() {
+  print('search');
+}
+
+_onTapMenu() {
+  print('menu');
+}
+
+////////////////////////////////////////////////////////////////////////////////
